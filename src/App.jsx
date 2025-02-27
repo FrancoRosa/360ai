3a0f65e9a90aa061d6f9f9fd9812ca152e82266d
FrancoRosa <km115.franco@gmail.com>
2025-02-27 11:23:51 -0500

    Add gps values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cam from "./components/CamDetection";
-import Controls from "./components/Controls";
 import GPS from "./components/GPS";
 import Config from "./components/Config";
 import useLocal from "./js/storage";
 import Navigation from "./components/Navigation";
-
+import { io } from "socket.io-client";
 function App() {
   const resolution = {
     width: 640,
@@ -16,6 +15,7 @@ function App() {
     beep: true,
     detection: true,
   });
+  const [gps, setGPS] = useState({});
   const [lines, setLines] = useLocal("lines", {
     vt1: resolution.width / 4,
     vt2: (3 * resolution.width) / 4,
@@ -25,13 +25,24 @@ function App() {
     hb: (3 * resolution.height) / 4,
   });
 
+  useEffect(() => {
+    const socket = io("http://localhost:8080");
+    socket.on("gpsData", (data) => {
+      setGPS(data);
+    });
+
+    // Handle connection errors
+    socket.on("connect_error", (err) => {
+      console.error("Connection error:", err);
+    });
+  }, []);
+
   return (
     <div className=" h-screen text-slate-900 bg-slate-400 dark:text-lime-400 dark:bg-slate-900 text-sm flex flex-col justify-around items-center">
       <div className="flex justify-around">
         {page === "main" && (
           <>
-            <Controls />
-            <GPS />
+            <GPS {...{ gps }} />
           </>
         )}
         {page === "config" && (
--- src/components/GPS.jsx
+++ src/components/GPS.jsx
@@ -1,37 +1,19 @@
-import { useEffect, useState } from "react";
-
 const Indicator = ({ label, value, unit }) => (
   <div className="text-center">
     <p className="text-xs ">{label}</p>
-    <h3 className="text-2xl ">{value}</h3>
+    <h3 className="text-2xl ">{value?.toFixed(1) || "0.0"}</h3>
     <p className="text-xs ">{unit}</p>
   </div>
 );
 
-const GPS = () => {
-  const [gps, setGps] = useState({
-    height: 100,
-    bearing: 45,
-    speed: 100,
-  });
-
-  useEffect(() => {
-    const plusOrMinus = () => (Math.random() < 0.5 ? -1 : 1);
-    const interval1 = setInterval(() => {
-      setGps((s) => ({
-        height: s.height + Math.floor(Math.random() * 1000 * plusOrMinus()),
-        bearing: s.bearing + Math.floor(Math.random() * 10 * plusOrMinus()),
-        speed: s.speed + Math.floor(Math.random() * 10 * plusOrMinus()),
-      }));
-    }, 3000);
-    return () => clearInterval(interval1);
-  }, []);
+const GPS = ({ gps }) => {
+  const { altitude, bearing, speed } = gps;
 
   return (
     <div className="flex gap-4">
-      <Indicator label="Height" value={gps.height} unit="ft ASL" />
-      <Indicator label="Bearing" value={gps.bearing} unit="degrees" />
-      <Indicator label="Speed" value={gps.speed} unit="mph" />
+      <Indicator label="Altitude" value={altitude} unit="ft ASL" />
+      <Indicator label="Bearing" value={bearing} unit="degrees" />
+      <Indicator label="Speed" value={speed} unit="mph" />
     </div>
   );
 };
