c672874dd0f8e5300dc4663b205874e5a83a19ec
FrancoRosa <km115.franco@gmail.com>
2025-03-04 13:46:40 -0500

    Enable webCam

--- src/components/CamDetection.jsx
+++ src/components/CamDetection.jsx
@@ -118,14 +118,14 @@ const Cam = ({ resolution, lines, config, page, setPerson }) => {
         </select>
 
         <div style={style}>
-          {/* <Webcam
+          <Webcam
             ref={webcamRef}
             style={{ position: "absolute", top: 0 }}
             videoConstraints={{
               deviceId,
               ...resolution,
             }}
-          /> */}
+          />
           <ROI resolution={resolution} top={true} warning={status[0]} />
           <ROI resolution={resolution} top={false} warning={status[1]} />
           <canvas ref={canvasRef} style={{ position: "absolute", top: 0 }} />
